222eae19bfd3b5b182429f8d7e951b93e208f14a
Nick <nicholasconnors@hotmail.ca>
2023-05-18 23:15:31 -0400

    Set Dredge path from file explorer, text box

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import "./App.css";
-
-// Import our custom CSS
 import './scss/styles.scss'
-
-// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import { open } from "@tauri-apps/api/dialog";
+import {useState} from 'react';
 
 function App() {
+  const [dredgePath, setDredgePath] = useState("");
+
+  const readFileContents = async () => {
+    try {
+      const selectedPath = await open({
+        multiple: false,
+        title: "Select DREDGE game folder",
+        directory: true
+      });
+      setDredgePath(selectedPath as string);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <body className="bg-dark text-light min-vh-100">
       <div className="h-100 w-100 container min-vh-100">
+        <br/>
         <h1 className="text-center">Dredge Mod Manager</h1>
         <br/>
         <div className="text-start">
             Dredge folder path:
         </div>
         <div>
-          <input type="text" id="dredge-path-text" className="w-75"></input>
-          <button className="w-25">...</button>
+          <input type="text" className="w-75" onChange={(e) => setDredgePath(e.target.value)} value={dredgePath}></input>
+          <button className="w-25" onClick={readFileContents}>...</button>
         </div>
 
         <br/>
