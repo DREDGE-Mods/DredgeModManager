965c509cf1b8d0485dc22d354b842c0b28cc454f
Nick <nicholasconnors@hotmail.ca>
2023-07-02 15:45:36 -0400

    Comment out alt play button for now

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -105,9 +105,11 @@ class SidebarPlay extends Component<{start: () => void}>
                     <button className="sidebar-play-main" id="play-main" onClick={this.props.start}>
                         <label htmlFor="play-main">play</label>
                     </button>
+                    {/* TODO: alt play button to run without mods
                     <button className="sidebar-play-alt" id="play-alt">
                         <label htmlFor="play-alt"></label>
                     </button>
+                    */}
                 </div>
             </div>
         )
