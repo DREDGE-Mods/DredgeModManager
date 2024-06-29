1dddf1bdf78a54ad206d814af674409982230866
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2024-06-29 15:38:41 +0100

    fix: introduce local state to config checkboxes to remove delay

--- src/components/settings/BooleanConfig.tsx
+++ src/components/settings/BooleanConfig.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 // ID here is case-sensitive attribute of WinchConfig, e.g., (EnableDeveloperConsole, DetailedLogSources)
 interface IBooleanConfigProps {
     id: string;
     label: string,
-    checked?: boolean,
+    checked: boolean,
     onUpdate: (id: string, value: boolean) => void
 }
 
 export const BooleanConfig = (props: IBooleanConfigProps) => {
+    const [checked, setChecked] = useState(props.checked);
+    const updateChecked = (value: boolean) => {
+        setChecked(value);
+        props.onUpdate(props.id, value);
+    }
     return <div>
-        <input id={props.id} name={props.id} type={"checkbox"} checked={props.checked}
-               onChange={(e) => {
-                   props.onUpdate(props.id, e.target.checked)
-               }}/>
+        <input id={props.id} name={props.id} type={"checkbox"} checked={checked}
+               onChange={(e) => updateChecked(e.currentTarget.checked)}/>
         <label htmlFor={props.id}>{props.label}</label>
     </div>
 }
